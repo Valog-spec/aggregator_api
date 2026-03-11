fea19bad502044235c68e8503330eab19c2592ce
Victor Sagalakov <v.sagalakov@botsarmy.tech>
2026-03-11 13:14:50 +0700

    fix(): lint

--- alembic/versions/1b3204d9a869_add_outbox_table.py
+++ alembic/versions/1b3204d9a869_add_outbox_table.py
@@ -8,9 +8,10 @@ Create Date: 2026-03-11 11:49:23.552921
 
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
+
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "1b3204d9a869"
